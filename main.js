87db6aa736e244176ee092a425c1919b96cda9fc
Administrator <327742525@qq.com>
2021-04-12 10:36:28 +0800

    封装重复代码

--- main.js
+++ main.js
@@ -23,44 +23,50 @@ let listenToMouse = (canvas) => {
   let lastPainting = []
 
   context.fillStyle = 'black'
-  context.fillStyle = 'none'
   context.lineWidth = 10
   context.lineCap = "round"
 
-
-  if (isTouchDevice) {
-    let x = e.touches[0].clientX
-    let y = e.touches[0].clientY
-    canvas.ontouchstart = (e) => {
+  let startDrawing = (x, y) => {
+    if (eraserToggle) {
+      functionToggle = true
+      context.clearRect(x - 5, y - 5, 10, 10)
+    } else {
+      functionToggle = true
       lastPainting = [x, y]
     }
+  }
+  let drawing = (x, y) => {
+    if (functionToggle) {
+      if (eraserToggle) {
+        context.clearRect(x - 5, y - 5, 10, 10)
+      } else {
+        drawLine(lastPainting[0], lastPainting[1], x, y)
+        lastPainting = [x, y]
+      }
+    }
+  }
+
+  if (isTouchDevice) {
+    canvas.ontouchstart = (e) => {
+      let x = e.touches[0].clientX
+      let y = e.touches[0].clientY
+      startDrawing(x, y)
+    }
     canvas.ontouchmove = (e) => {
-      drawLine(lastPainting[0], lastPainting[1], x, y)
-      lastPainting = [x, y]
+      let x = e.touches[0].clientX
+      let y = e.touches[0].clientY
+      drawing(x, y)
     }
   } else {
     canvas.onmousedown = (e) => {
       let x = e.clientX
       let y = e.clientY
-      if (eraserToggle) {
-        functionToggle = true
-        context.clearRect(x - 5, y - 5, 10, 10)
-      } else {
-        functionToggle = true
-        lastPainting = [x, y]
-      }
+      startDrawing(x, y)
     }
     canvas.onmousemove = (e) => {
       let x = e.clientX
       let y = e.clientY
-      if (functionToggle) {
-        if (eraserToggle) {
-          context.clearRect(x - 5, y - 5, 10, 10)
-        } else {
-          drawLine(lastPainting[0], lastPainting[1], e.clientX, e.clientY)
-          lastPainting = [e.clientX, e.clientY]
-        }
-      }
+      drawing(x, y)
     }
     canvas.onmouseup = () => {
       functionToggle = false
@@ -72,6 +78,7 @@ let listenToMouse = (canvas) => {
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
+
   }
 
   eraser.onclick = () => {
